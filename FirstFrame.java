e3b895c3d354df087ad4f3cfe4877ce004bf247a
Thandanani2 <167040599+Thandanani2@users.noreply.github.com>
2024-08-20 19:50:59 +0200

    Add files via upload

---
+++ FirstFrame.java
@@ -0,0 +1,16 @@
+
+package za.ac.tut;
+
+import javax.swing.JFrame;
+
+public class FirstFrame extends JFrame{
+
+    public FirstFrame() {
+        this.setTitle("My first Understanding GUI");
+        this.setSize(250,250);
+        this.setResizable(true);
+        this.setVisible(true);
+    }
+    
+    
+}
